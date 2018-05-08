bcdda2104219b78af95791af790a60ef5f609a95
weberke <weberk@mountunion.edu>
2018-05-08 00:06:11 -0400

    Cleaned up createModuli and made sieve smaller.

--- createModuli.cpp
+++ createModuli.cpp
@@ -29,56 +29,58 @@
 
 using namespace GmpCuda;
 
-const size_t MAX_NUM_MODULI = 1 << 27;  //  can go up to 1<<27 with current declarations.
-uint32_t moduliList[MAX_NUM_MODULI];
-uint64_t mInvList[MAX_NUM_MODULI];
-
-const unsigned int SIEVE_SZ = 1 << 31;  //  Should handle all 32 bit primes.
-static char sieve[SIEVE_SZ];
 
 using namespace std;
 
+//  Returns gcd(x, y).
+//  Precondition: both x and y are odd.
+static inline uint32_t gcd (uint32_t x, uint32_t y)
+{
+  while (x != y)
+    {
+      if (x > y)
+        {
+          x -= y;
+          while (x % 2 == 0)
+            x /= 2;
+        }
+      else
+        {
+          y -= x;
+          while (y % 2 == 0)
+            y /= 2;
+        }
+    }
+  return x;
+}
+
 static void mark_composite(uint32_t limit, char* sieve, size_t sieve_sz)
 {
-  uint32_t d = static_cast<uint32_t>(floor(sqrt((double)limit)));
+  uint32_t d = static_cast<uint32_t>(floor(sqrt(static_cast<double>(limit))));
 
-  if ((d&1) == 0)
+  if (d % 2 == 0)
     d -= 1;                /*  Make d odd.  */
 
   while (d > 2)
     {
-      uint32_t g = uint32_t{3}*5*7*11*13*17*19*23*29;
-      uint32_t tmp = d;
-      while (g != tmp)
-          if (g > tmp)
-            {
-              g -= tmp;
-              while ((g&1) == 0)
-                  g >>= 1;
-            }
-          else
-            {
-              tmp -= g;
-              while ((tmp&1) == 0)
-                  tmp >>= 1;
-            }
+      uint32_t g = gcd(d, uint32_t{3}*5*7*11*13*17*19*23*29);
       switch (g)
         {
-          size_t i, end;
-          case  3: case  5: case  7: case 11:
-          case 13: case 17: case 19: case 23: case 29:
-              if (g != d)
-                  break;
-          case 1:
-              i = limit % d;
-              if (i & 1)
-                  i += d;
-              i >>= 1;
-              end = (limit - d)>>1;
-              if (sieve_sz < end)
-              end = sieve_sz;
-              while (i < end)
-                  sieve[i] = !0, i += d;
+        case  3: case  5: case  7: 
+        case 11: case 13: case 17: 
+        case 19: case 23: case 29:
+          if (d != g)
+            break;  // d is a multiple of small prime--ignore.
+        case 1:
+          size_t i = limit % d;
+          if (i % 2)
+            i += d;
+          i /= 2;
+          size_t end = (limit - d) / 2;
+          if (sieve_sz < end)
+            end = sieve_sz;
+          while (i < end)
+            sieve[i] = !0, i += d;
         }
       d -= 2;
     }
@@ -95,70 +97,48 @@ static void mark_composite(uint32_t limit, char* sieve, size_t sieve_sz)
 
 size_t primes(uint32_t * list, size_t n, int k)
 {
+  static constexpr uint32_t SIEVE_SZ = 1 << 30; //  Should handle all k-bit odd primes for k <= 32. 
+  static char sieve[SIEVE_SZ];                
   uint32_t limit = (uint64_t{1} << k) - 1;
   uint32_t lowerLimit = 1 << (k - 1);
 
-  size_t sieve_sz, inv_density;
+  size_t sieve_sz = SIEVE_SZ;
   
-  if ((limit&1) == 0)                /*  If limit is even,  */
-      limit -= 1;                /*  make limit next smaller odd number.  */
-
   if (n == 0 || limit < 3)
       return n;
 
-  inv_density = ceil(log((double)limit)/2.0);
+   /*  Now strike non-primes from the sieve and harvest primes.  */
+  if (sieve_sz > limit/2)
+      sieve_sz = limit/2;
 
-  /*  The sieve should be large enough to find n primes.  */
+  memset(sieve, 0, sieve_sz);
 
-  sieve_sz = (n > static_cast<uint32_t>(-1)/inv_density) ? static_cast<uint32_t>(-1) : n * inv_density;
-  
-  if (sieve_sz > SIEVE_SZ)
+  mark_composite(limit, sieve, sieve_sz);
+
+  /*  Harvest primes.  */
+  for (size_t i = 0; i < sieve_sz; i++)
     {
-      cerr << "Not enough memory for sieve"  << endl;
-      std::exit(1);
+      if (sieve[i])
+        continue;                         /*  Composite.  */
+      *list = limit - (i << 1);
+      if (*list++ < lowerLimit)
+        return n;
+      n -= 1;
+      if (n == 0)
+        return n;
     }
-
-  /*  Now strike non-primes from the sieve and harvest primes.  */
-  do
-    {
-      size_t i;
-      uint32_t d;
-
-      if (sieve_sz > limit/2)
-          sieve_sz = limit/2;
-
-      memset(sieve, 0, sieve_sz);
-
-      mark_composite(limit, sieve, sieve_sz);
-
-      /*  Harvest primes.  */
-      for (i = 0; i < sieve_sz; i++)
-        {
-          if (sieve[i])
-            continue;                         /*  Composite.  */
-          *list = limit - (i << 1);
-          if (*list++ < lowerLimit)
-            return n;
-          n -= 1;
-          if (n == 0)
-            return n;
-        }
-
-      limit -= 2*sieve_sz;
-      if (sieve_sz/n > inv_density)
-          sieve_sz = n * inv_density;
-    }
-  while (limit >= 3);
 
   return n;
 }
 
 int main(int argc, char *argv[])
 {
-  size_t   mListSize = MAX_NUM_MODULI - primes(moduliList, MAX_NUM_MODULI, L);
+  static constexpr size_t MAX_NUM_MODULI = 100 * 1000 * 1000;  //  can go up to 1<<27 with current declarations.
+  static uint32_t moduliList[MAX_NUM_MODULI];
+  size_t mListSize = MAX_NUM_MODULI - primes(moduliList, MAX_NUM_MODULI, L);
 
-  size_t mListSizeOriginal = mListSize;
-  mListSize = 0;
+  cerr << "There are " << mListSize << " " << L << "-bit primes." << endl;
+
   mpz_t FC, J, DJ_FC, Qcr, Ncr;
   mpz_init_set_ui(FC,  2);
   mpz_pow_ui(FC, FC, W + L - 1);  //  FC <-- 2^(W + L - 1)
@@ -166,7 +146,20 @@ int main(int argc, char *argv[])
   mpz_init(DJ_FC);
   mpz_init(Qcr);
   mpz_init(Ncr);
-  for (size_t i = 0; i < mListSizeOriginal; i += 1)
+  
+  cout   << "//  AUTOMATICALLY GENERATED by createModuli: do not edit" << endl
+         << endl
+         << "//  A list of " << L << "-bit primes, selected so that DBM_a(N, J) will always be accurate." << endl
+         << "//  See Cavagnino & Werbrouck," << endl
+         << "//      Efficient Algorithms for Integer Division by Constants Using Multiplication," << endl
+         << "//      The Computer Journal, Vol. 51 No. 4, 2008." << endl
+         << endl
+         << "#include \"GmpCudaDevice.h\"" << endl
+         << "__device__ const GmpCuda::modulus_t GmpCuda::moduliList[] = " << endl
+         << "{" << endl;
+  
+  size_t numUsable = 0;
+  for (size_t i = 0; i < mListSize; i += 1)
     {
       uint32_t D = moduliList[i];
       mpz_fdiv_q_ui(J, FC, D);
@@ -178,38 +171,19 @@ int main(int argc, char *argv[])
       mpz_sub_ui(Ncr, Ncr, 1);      //  Ncr <-- Qcr * D - 1
       if (mpz_sizeinbase(Ncr, 2) > W)
         {
-          moduliList[mListSize] = moduliList[i];    //  Compress list.
-          mpz_export(mInvList + mListSize++, NULL, -1, sizeof(uint64_t), 0, 0, J);
+          uint64_t inverse;
+          mpz_export(&inverse, NULL, -1, sizeof(uint64_t), 0, 0, J);
+          cout << "\t{" << moduliList[i] << ", " << inverse << "}," << endl;
+          numUsable += 1;
+          if (numUsable < NUM_MODULI)
+            continue;
+          cout << "};" << endl;
+          return 0;  
         }
     }
 
-  cerr   << "There are " << mListSize << " usable " << L << "-bit primes." << endl;
-  cerr   << "Percentage of primes usable as moduli = " << 100 * mListSize/mListSizeOriginal << endl;
-  
-  cout   << "//  AUTOMATICALLY GENERATED by createModuli: do not edit" << endl
-         << endl;
-  cout   << "//  A list of " << L << "-bit primes, selected so that DBM_a(N, J) will always be accurate." << endl
-         << "//  See Cavagnino & Werbrouck," << endl
-         << "//      Efficient Algorithms for Integer Division by Constants Using Multiplication," << endl
-         << "//      The Computer Journal, Vol. 51 No. 4, 2008." << endl
-         << endl
-         << "#include <stdint.h>" << endl
-         << "#include \"GmpCudaDevice.h\"" << endl
-         << "using namespace GmpCuda;" << endl
-//         << "const int GmpCuda::NUM_MODULI = " << mListSize << ";" << endl
-         << "__device__ const modulus_t GmpCuda::moduliList[] = " << endl
-         << "{" << endl;
-         
-  if (mListSize < NUM_MODULI)
-    {
-      cerr << "There are only " << mListSize << " usable " << L << "-bit primes; ";
-      cerr << NUM_MODULI << " are required." << endl;
-      std::exit(1);
-    }
-    
-  for (size_t i = 0; i < NUM_MODULI; i += 1)
-    cout << "\t{" << moduliList[i] << ", " << mInvList[i] << "}," << endl;
-    
-  cout   << "};" << endl;
+  cerr << "There are only " << numUsable << " usable " << L << "-bit primes; ";
+  cerr << NUM_MODULI << " are required." << endl;
+  return 1;
   
 }
