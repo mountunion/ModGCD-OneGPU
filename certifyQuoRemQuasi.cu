7e6aa3a8768f426a71356f3738b8b56bd26434a9
weberke <weberk@mountunion.edu>
2018-08-01 11:28:13 -0400

    Cleaned up certifyquoRemQuasi.cu.

--- certifyQuoRemQuasi.cu
+++ certifyQuoRemQuasi.cu
@@ -1,9 +1,16 @@
 /*  certifyQuoRemQuasi.cu
 
-    This program will certify that quoRem<QUASI>(xf, yf) works correctly, 
-    as long as xf and yf are integers, 1 <= xf < 2 * FLOAT_THRESHOLD, 
-    and 1 <= yf < FLOAT_THRESHOLD, by testing the function on all possible 
-    input satisfying the preconditions.
+    This program will certify that quoRem<QUASI>(rf, xf, yf) works correctly.
+    Preconditions: 
+      xf and yf are integers
+      if xf > 1.0 and yf > 1.0, then xf != yf
+      1 <= xf < FLOAT_THRESHOLD * 2
+      1 <= yf < FLOAT_THRESHOLD
+    Postcondition tested:
+      quoRem<QUASI>(rf, xf, yf) returns in rf a value satisfying 
+        0.0f <= rf < yf     when 0      <  xf < yf * 2
+        0.0f <= rf < yf * 2 when yf * 2 <= xf < FLOAT_THRESHOLD * 2
+      for all possible input xf and yf satisfying the preconditions.
     
     K. Weber
     20-July, 2018.
@@ -20,37 +27,25 @@ __device__ inline void checkRange(bool* fail, uint32_t xInit, uint32_t xLimit, f
   for (uint32_t x = xInit; x < xLimit; x += 1)
     {
       float rf;
-      float qf = quoRem<QUASI>(rf, __uint2float_rz(x), yf);
+      float xf = __uint2float_rz(x);
+      float qf = quoRem<QUASI>(rf, xf, yf);
       if (0.0f <= rf && rf < limit)
         continue;
       *fail = true;
-      printf("Failed for x == %u and yf == %f: qf == %f, rf = %f\n", x, yf, qf, rf);
+      printf("Failed for xf == %f and yf == %f: qf == %f, rf = %f\n", xf, yf, qf, rf);
     }
 }
 
 __global__ void kernel(bool* fail)
 {
-  //  Check y == 1 first. (Possibly not necessary--analysis indicates it should always be OK.)
-  float yf = __uint2float_rz(1);
-  for (uint32_t x = blockIdx.x * blockDim.x + threadIdx.x  + 1; x < 2 *FLOAT_THRESHOLD; x += blockDim.x * gridDim.x)
-    {
-      float rf;
-      float qf = quoRem<QUASI>(rf, __uint2float_rz(x), yf);
-      if (rf == 0.0f)
-        continue;
-      *fail = true;
-      printf("Failed for x == %u and yf == %f: qf == %f, rf = %f\n", x, yf, qf, rf);
-    }
   //  Now test all divisors 1 < y < FLOAT_THRESHOLD.
-  for (uint32_t y = blockIdx.x * blockDim.x + threadIdx.x + 2; y < FLOAT_THRESHOLD; y += blockDim.x * gridDim.x)
+  for (uint32_t y = blockIdx.x * blockDim.x + threadIdx.x + 1; y < FLOAT_THRESHOLD; y += blockDim.x * gridDim.x)
     {
-      yf = __uint2float_rz(y);
+      float yf = __uint2float_rz(y);
       //  Now make sure quoRem<QUASI> satisfies postconditions.
-      // for 1 <= x < 2 * y && x != y, then 0 <= rf < yf.
-      checkRange(fail, 1,         y, yf, yf); // Possibly not necessary--analysis indicates it should always be OK.
-      checkRange(fail, y + 1, 2 * y, yf, yf); // Possibly not necessary--analysis indicates it should always be OK.
-      // for 2 * y <= x < 2 * FLOAT_THRESHOLD, then 0 <= rf < 2 * yf.
-      checkRange(fail, 2 * y, 2 * FLOAT_THRESHOLD, yf, 2 * yf);  // Necessary.  Analysis indicates q could be too high.
+      checkRange(fail, 1, max(y, 2), yf, yf);  //  Checks quoRem<QUASI>(rf, 1.0f, 1.0f).
+      checkRange(fail, y + 1, y * 2, yf, yf);
+      checkRange(fail, y * 2, FLOAT_THRESHOLD * 2, yf, 2 * yf);
     }
 }
 
